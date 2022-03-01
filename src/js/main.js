cffd8912a7ac5f5b5b8bf31a07d6f593d7e762b0
magu12 <maggubadov@gmail.com>
2022-03-01 16:39:19 +0300

    adaptations

--- src/js/main.js
+++ src/js/main.js
@@ -24,15 +24,23 @@ $(document).ready(function ($) {
     $(this).find('.feedback__check').toggleClass('active');
   });
 
-  $('.card__thumb').on('click', function(){
+  $('.card__thumb').on('click', function () {
     $(this).addClass('active').siblings().removeClass('active');
     $('.card__item').removeClass('active').eq($(this).index()).addClass('active');
   });
-/* 
-  $('.bid__item').hover(function(){
-    $(this).find('.bid__company.original').addClass('active');
-    setTimeout('')
-  }); */
+
+  $('.bid__item').hover(function () {
+    $(this).find('.bid__company').css('animation-duration', ($(this).find('.bid__company').width() / 350).toFixed(1) + 's');
+  });
+
+  $('.nav__open').on('click', function () {
+    $(this).toggleClass('active');
+    $('.nav-mobile').toggleClass('active');
+  });
+
+  $('.filter__title').on('click', function(){
+    $(this).closest('.filter').toggleClass('active');
+  })
 
   var cardThumbsSwiper = new Swiper(".card__slider-thumbs", {
     spaceBetween: 15,
@@ -60,7 +68,14 @@ $(document).ready(function ($) {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
-    },
+    },breakpoints: {
+      300: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 3,
+      }
+    }
   });
 
   var projectSwiper = new Swiper(".project__slider", {
@@ -72,7 +87,14 @@ $(document).ready(function ($) {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
-    },
+    },breakpoints: {
+      300: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 3,
+      }
+    }
   });
 
 
@@ -80,114 +102,114 @@ $(document).ready(function ($) {
 
 
   // range-slider
-  var settings = {
-    visible: 0, 
-    theme: {
-      backgroud: "rgba(0,0,0,.9)",
-    },
-    CSSVarTarget: document.querySelector('.range-slider'),
-    knobs: [
-      "Thumb",
-      {
-        cssVar: ['thumb-size', 'px'],
-        label: 'thumb-size',
-        type: 'range',
-        min: 6, max: 33 //  value: 16,
+  /*   var settings = {
+      visible: 0,
+      theme: {
+        backgroud: "rgba(0,0,0,.9)",
       },
-      "Value",
-      {
-        cssVar: ['value-active-color'], // alias for the CSS variable
-        label: 'value active color',
-        type: 'color',
-        value: 'white'
-      },
-      {
-        cssVar: ['value-background'], // alias for the CSS variable
-        label: 'value-background',
-        type: 'color',
-      },
-      {
-        cssVar: ['value-background-hover'], // alias for the CSS variable
-        label: 'value-background-hover',
-        type: 'color',
-      },
-      {
-        cssVar: ['primary-color'], // alias for the CSS variable
-        label: 'primary-color',
-        type: 'color',
-      },
-      {
-        cssVar: ['value-offset-y', 'px'],
-        label: 'value-offset-y',
-        type: 'range', value: 5, min: -10, max: 20
-      },
-      "Track",
-      {
-        cssVar: ['track-height', 'px'],
-        label: 'track-height',
-        type: 'range', value: 8, min: 6, max: 33
-      },
-      {
-        cssVar: ['progress-radius', 'px'],
-        label: 'progress-radius',
-        type: 'range', value: 20, min: 0, max: 33
-      },
-      {
-        cssVar: ['progress-color'], // alias for the CSS variable
-        label: 'progress-color',
-        type: 'color',
-        value: '#EEEEEE'
-      },
-      {
-        cssVar: ['fill-color'], // alias for the CSS variable
-        label: 'fill-color',
-        type: 'color',
-        value: '#0366D6'
-      },
-      "Ticks",
-      {
-        cssVar: ['show-min-max'],
-        label: 'hide min/max',
-        type: 'checkbox',
-        value: 'none'
-      },
-      {
-        cssVar: ['ticks-thickness', 'px'],
-        label: 'ticks-thickness',
-        type: 'range',
-        value: 1, min: 0, max: 10
-      },
-      {
-        cssVar: ['ticks-height', 'px'],
-        label: 'ticks-height',
-        type: 'range',
-        value: 5, min: 0, max: 15
-      },
-      {
-        cssVar: ['ticks-gap', 'px'],
-        label: 'ticks-gap',
-        type: 'range',
-        value: 5, min: 0, max: 15
-      },
-      {
-        cssVar: ['min-max-x-offset', '%'],
-        label: 'min-max-x-offset',
-        type: 'range',
-        value: 10, step: 1, min: 0, max: 100
-      },
-      {
-        cssVar: ['min-max-opacity'],
-        label: 'min-max-opacity',
-        type: 'range', value: .5, step: .1, min: 0, max: 1
-      },
-      {
-        cssVar: ['ticks-color'], // alias for the CSS variable
-        label: 'ticks-color',
-        type: 'color',
-        value: '#AAAAAA'
-      },
-    ]
-  }
+      CSSVarTarget: document.querySelector('.range-slider'),
+      knobs: [
+        "Thumb",
+        {
+          cssVar: ['thumb-size', 'px'],
+          label: 'thumb-size',
+          type: 'range',
+          min: 6, max: 33 //  value: 16,
+        },
+        "Value",
+        {
+          cssVar: ['value-active-color'], // alias for the CSS variable
+          label: 'value active color',
+          type: 'color',
+          value: 'white'
+        },
+        {
+          cssVar: ['value-background'], // alias for the CSS variable
+          label: 'value-background',
+          type: 'color',
+        },
+        {
+          cssVar: ['value-background-hover'], // alias for the CSS variable
+          label: 'value-background-hover',
+          type: 'color',
+        },
+        {
+          cssVar: ['primary-color'], // alias for the CSS variable
+          label: 'primary-color',
+          type: 'color',
+        },
+        {
+          cssVar: ['value-offset-y', 'px'],
+          label: 'value-offset-y',
+          type: 'range', value: 5, min: -10, max: 20
+        },
+        "Track",
+        {
+          cssVar: ['track-height', 'px'],
+          label: 'track-height',
+          type: 'range', value: 8, min: 6, max: 33
+        },
+        {
+          cssVar: ['progress-radius', 'px'],
+          label: 'progress-radius',
+          type: 'range', value: 20, min: 0, max: 33
+        },
+        {
+          cssVar: ['progress-color'], // alias for the CSS variable
+          label: 'progress-color',
+          type: 'color',
+          value: '#EEEEEE'
+        },
+        {
+          cssVar: ['fill-color'], // alias for the CSS variable
+          label: 'fill-color',
+          type: 'color',
+          value: '#0366D6'
+        },
+        "Ticks",
+        {
+          cssVar: ['show-min-max'],
+          label: 'hide min/max',
+          type: 'checkbox',
+          value: 'none'
+        },
+        {
+          cssVar: ['ticks-thickness', 'px'],
+          label: 'ticks-thickness',
+          type: 'range',
+          value: 1, min: 0, max: 10
+        },
+        {
+          cssVar: ['ticks-height', 'px'],
+          label: 'ticks-height',
+          type: 'range',
+          value: 5, min: 0, max: 15
+        },
+        {
+          cssVar: ['ticks-gap', 'px'],
+          label: 'ticks-gap',
+          type: 'range',
+          value: 5, min: 0, max: 15
+        },
+        {
+          cssVar: ['min-max-x-offset', '%'],
+          label: 'min-max-x-offset',
+          type: 'range',
+          value: 10, step: 1, min: 0, max: 100
+        },
+        {
+          cssVar: ['min-max-opacity'],
+          label: 'min-max-opacity',
+          type: 'range', value: .5, step: .1, min: 0, max: 1
+        },
+        {
+          cssVar: ['ticks-color'], // alias for the CSS variable
+          label: 'ticks-color',
+          type: 'color',
+          value: '#AAAAAA'
+        },
+      ]
+    }
   
-  new Knobs(settings)
+    new Knobs(settings) */
 })
